f70f184ecf51e3de2ef55cd65be8a808566da37e
Asif-Hassan-Rahul <63399934+Asif-Hassan-Rahul@users.noreply.github.com>
2022-02-17 03:04:58 +0600

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.web')
 
 @section('custom_css')
+    <style>
+        .single-simple-steps:hover{
+            box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+        }
 
+
+    </style>
 @endsection
 
 @section('content')
